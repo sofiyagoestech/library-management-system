43236e5f63112d54292c98e7db4f6b38779ce2aa
sofiyagoestech <111189839+sofiyagoestech@users.noreply.github.com>
2026-01-22 21:43:32 +0000

    Automate start and save sessions, optimise remove/search and save features

--- library.py
+++ library.py
@@ -1,95 +1,88 @@
+"""Library programme"""
+
 class Library:
+
     def __init__(self):
-        self.items = []
-        self.filename = "new_library.txt"
+        self.filename = "library1.txt"
+        self.items = self.start_library()
+
+    def __enter__(self):
+        print("Loading your library".center(50, "="))
+        print(self.items)
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print("Saving new items".center(50, "="))
+        print(self.save_items())
 
     def start_library(self):
-        """By starting the programme it displays the current books from txt file"""
+        """By starting the programme it should display the current books from txt file"""
         try:
             with open(self.filename, "r") as file:
-                lines = file.readlines()
                 library = []
-                for i, line in enumerate(lines, start=1):
-                    library.append(f"{i}. {line.strip()}")
+                for line in file:
+                    line = line.strip().split("|")
+                    library.append(tuple(line))
 
-            return "\n".join(library)
+            return library
 
         except FileNotFoundError:
-            return "Library file not found"
+            return []
 
 
     def add_item(self, item):
         """Add an item to the library list"""
-        info = []
-        for i in item.values():
-            info.append(i)
-        self.items.append(tuple(info))
+        self.items.append(item)
         return self.items
 
     def save_items(self):
-        """Save a book from list to txt file.
-        If list is empty, error message: No books found
-        Otherwise, add title, author, year into txt file and display success message: Book added successfully"""
-
+        """Save a book to the library list."""
         if self.items:
-            with open(self.filename, "a") as file:
+            with open(self.filename, "w") as file:
                 for item in self.items:
                     title, name, year = item
                     file.write(f"{title}|{name}|{year}\n")
-
-            return True, "Book added successfully"
+            return "Book(s) added successfully"
         else:
-            return False, "No books found"
+            return "No books found"
 
 
-    def remove_item(self, item):
+    def remove_item(self,**kwargs):
+        removed = []
+        for remove_item in self.search_item(**kwargs):
+            if remove_item in self.items:
+                self.items.remove(remove_item)
+                removed.append(remove_item)
+            else:
+                return "Item not found"
+        return f"{removed} removed"
 
-        """Remove book from txt file.
-        User input book title or author's name. Function searches for matches and removes them from txt file.
-        If found, success message: Book removed successfully.
-        Otherwise, error message: No book found"""
 
-        with open(self.filename, "r") as file:
-            lines = file.readlines()
-        with open(self.filename, "w") as file:
-            match = False
-            for line in lines:
-                if item not in line:
-                    file.write(line)
-                else:
-                    match = True
-                    continue
-        if match:
-            return True, "Book removed successfully"
-        else:
-            return False, "No book found"
-
-    def search_item(self,item):
-        """Search book in txt file and display it if found. 
+    def search_item(self, **kwargs):
+        """Search book in list and display it if found.
         Otherwise, error message: No book found"""
         search = []
-        with open(self.filename, "r") as file:
-            lines = file.readlines()
-            match = False
-            for line in lines:
-                if item in line:
-                    search.append(line)
-                    match = True
-                else:
+        if kwargs.get("title"):
+            for item in self.items:
+                title, name, year = item
+                if title.lower() == kwargs["title"].lower():
+                    search.append(item)
 
-                    continue
-        if not match:
-            return False, "No book found"
+        elif kwargs.get("author"):
+            for item in self.items:
+                title, name, year = item
+                if name.lower() == kwargs["author"].lower():
+                    search.append(item)
         else:
-            return "\n".join(search)
+            return "No book matching your criteria"
+
+        if not search:
+            return "No book found"
+        return search
 
 class Book:
-    def __init__(self):
-        self.book = {}
+    def __init__(self, title, author, year):
+        self.book = title, author, year
 
-    def create_book(self):
-        """Creating book from user input and returning it in dict format"""
-        self.book["title"] = input("Enter title: ")
-        self.book["author"] = input("Enter author: ")
-        self.book["year"] = input("Enter year: ")
-        return self.book
+    def __str__(self):
+        return f"{self.book[0]}|{self.book[1]}|{self.book[2]}"
